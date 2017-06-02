4108d659fe459afa290850c69807da966c06ea2a
buoyantair <buoyantair@gmail.com>
2017-06-02 16:40:55 +0530

    Woo! I was able to read and write data to mongodb!

--- additional01/app.js
+++ additional01/app.js
@@ -2,19 +2,46 @@ var express = require('express'),
 	MongoClient = require('mongodb').MongoClient,
 	app = express(),
 	engines = require('consolidate'),
-	assert = require('assert')
+	assert = require('assert'),
+	bodyParser = require('body-parser');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handler for internal server errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
+}
 
 MongoClient.connect('mongodb://localhost:27017', function(err, db){
 	assert.equal(null, err);
 	console.log('Successfully connected to mongodb');
-
-	app.use('html', engines.nunjucks);
-	app.use('view engine', 'html');
-	app.use('views', __dirname + '/views');
-	app.use(express.bodyParser());
-	app.use(app.router);
-
 	app.get('/', function(req, res){
-		res.render('index');
+		db.collection('posts').find({}).toArray(function(err, docs){
+			res.render('index', {'posts': docs})
+		})
+	})
+
+		app.post('/add', function(req, res, next){
+			var post = req.body.post;
+			if(post == ''){
+				next('Please provide an entry!')
+			} else {
+				db.collection('posts').insertOne({'post': post}, function(err,r){
+					assert.equal(null, err);
+					res.redirect('/');
+				})
+			}
+		});
+
+
+	app.use(errorHandler);
+	var server = app.listen(3000, function(){
+		var port = server.address().port;
+		console.log("Now listening at port %s", port);
 	})
 })
